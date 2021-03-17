bbe85ad6f866fd8e50e22d9b00b3c0613d4e4a04
Sonali Survase <ssurvase@techathalon.com>
2021-03-17 13:27:18 +0530

    init

---
+++ themeChanger/src/main/java/com/ssurvase/themechanger/ChangeTheme.java
@@ -0,0 +1,11 @@
+package com.ssurvase.themechanger;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ChangeTheme {
+    public void showToast(Context context){
+        Toast.makeText(context, "adsfasdf", Toast.LENGTH_SHORT).show();
+    }
+
+}
